900b1a95040b2b9da6426ff01e3cdc874d279cba
atharva201103 <131349847+atharva201103@users.noreply.github.com>
2024-08-25 00:08:44 +0530

    string slicing

--- stringslicing.py
+++ stringslicing.py
@@ -1,21 +1,21 @@
-# #simple slicing of string in python
-# print("\nsimple slicing")
-# name="Atharva Jawalkar"
-# print(name[0:7])
-# print(name[8:16])
+#simple slicing of string in python
+print("\nsimple slicing")
+name="Atharva Jawalkar"
+print(name[0:7])
+print(name[8:16])
 
-# #positive slicing with special pattern
-# print("\nMpositiveSlicing with special pattern")
-# x="Hello World"
-# print(x[0:5:4])
-# print(x[6:11:4])
-# print(x[4:8:3])
-# print(x[3:10:3])
+#positive slicing with special pattern
+print("\nMpositiveSlicing with special pattern")
+x="Hello World"
+print(x[0:5:4])
+print(x[6:11:4])
+print(x[4:8:3])
+print(x[3:10:3])
 
-# #negative slicing with special pattern
-# print("\nnegative slicing with special pattern")
-# x="abcdefgh"
-# print(x[-2:-5:-1])
-# print(x[-1:-6:-2])
-# print(x[-1:-6:-4])
-# print(x[-6:-9:-1])
+#negative slicing with special pattern
+print("\nnegative slicing with special pattern")
+x="abcdefgh"
+print(x[-2:-5:-1])
+print(x[-1:-6:-2])
+print(x[-1:-6:-4])
+print(x[-6:-9:-1])
