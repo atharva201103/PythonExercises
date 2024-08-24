8c3e06ccb7ed97b6d618da93fabdc717c942277f
atharva201103 <131349847+atharva201103@users.noreply.github.com>
2024-08-25 00:12:40 +0530

    type casting int to float

---
+++ typecasting(int).py
@@ -0,0 +1,27 @@
+# conversion of int to float
+print("conversion of int into float")
+a=10
+f=float(a)
+print(type(f))
+
+#conversion of int to str
+print("conversion of int to str")
+s=str(a)
+print(type(s))
+
+#conversion of int to bool
+print("conversion of int to bool")
+b=bool(a)
+print(type(b))
+
+#conversion of int to complex
+print("conversion of int to complex")
+c=complex(a)
+print(type(c))
+
+
+
+
+
+
+
